8fe04e2f3471a77cfd0f4f98ecc18f4ceb44ef6c
Edgars <the1edgars@gmail.com>
2024-10-25 09:58:47 +0300

    chapter 2 in Build Asteroids

--- main.py
+++ main.py
@@ -9,21 +9,34 @@ x = SCREEN_WIDTH / 2
 y = SCREEN_HEIGHT / 2
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    
     player = Player(x, y)
     dt = 0
     print('Starting asteroids!')
-    #print(f'Screen width: {SCREEN_WIDTH}')
-    #print(f'Screen height: {SCREEN_HEIGHT}')
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for thing in updatable:
+            thing.update(dt)                       
+        
         screen.fill(000)
-        player.draw(screen)
+
+        for thing in drawable:
+            thing.draw(screen) 
+
         pygame.display.flip()
-        player.update(dt)
+        
         dt = clock.tick(60)/1000
         
 if __name__ == "__main__":
